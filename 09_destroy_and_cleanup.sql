13fcc9d6081c6cc4b88086c3c4437c883e7007dc
IshwarBhat <ishwrbhat@gmail.com>
2022-04-24 16:14:32 -0700

    modify drops to check if exists before

--- 09_destroy_and_cleanup.sql
+++ 09_destroy_and_cleanup.sql
@@ -1,28 +1,28 @@
 ALTER TABLE MedicalHistory
-  DROP CONSTRAINT FK_MedicalHistory_Patient;
+  DROP CONSTRAINT IF EXISTS FK_MedicalHistory_Patient;
 
 ALTER TABLE Visit
-  DROP CONSTRAINT FK_Visit_Patient,
+  DROP CONSTRAINT IF EXISTS FK_Visit_Patient,
     FK_Visit_Doctor,
     FK_Visit_Room;
 
 ALTER TABLE Prescription
-  DROP CONSTRAINT FK_Prescription_Visit,
+  DROP CONSTRAINT IF EXISTS FK_Prescription_Visit,
     FK_Prescription_Drug;
 
 -- Drop depending objects first
-DROP VIEW vw_DoctorPrescriptionDrugs;
-DROP FUNCTION GetAgeFromDOB;
+DROP VIEW IF EXISTS vw_DoctorPrescriptionDrugs;
+DROP FUNCTION IF EXISTS GetAgeFromDOB;
 
 -- Drop users
 DROP USER IF EXISTS ddladmin;  
 
 -- Finally Drop the Tables
 -- WARNING: THIS CANNOT BE UNDONE!
-DROP TABLE Patient;
-DROP TABLE Doctor;
-DROP TABLE MedicalHistory;
-DROP TABLE Room;
-DROP TABLE Drug;
-DROP TABLE Visit;
-DROP TABLE Prescription;
+DROP TABLE IF EXISTS Patient;
+DROP TABLE IF EXISTS Doctor;
+DROP TABLE IF EXISTS MedicalHistory;
+DROP TABLE IF EXISTS Room;
+DROP TABLE IF EXISTS Drug;
+DROP TABLE IF EXISTS Visit;
+DROP TABLE IF EXISTS Prescription;
